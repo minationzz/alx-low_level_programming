5a44f795409f91e1a947ab91cccc56b8ce8fc909
usama <usamamoody@gmail.com>
2022-04-08 00:47:49 +0300

    all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+
+{
+
+printif("size of char: %lu", sizeof(char)\n);
+printif("size of int: %lu", sizeof(int)\n);
+printif("size of double: %lu", sizeof(double)\n);
+printif("size of float: %lu", sizeof(float)\n);
+
+return(0);
+}
