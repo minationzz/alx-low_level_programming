d141f9b08b718615f0acd893bc10db02ffed3904
usama <usamamoody@gmail.com>
2022-04-09 02:01:36 +0300

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.io>
+
+int main(void)
+/**
+ * main - initiates the program
+ *
+ * return: 0 successful
+ *
+ */
+{
+	char ch;
+
+	ch = 'a';
+
+	for
+	(ch <= 'z');
+	{
+	putchar(ch);
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
