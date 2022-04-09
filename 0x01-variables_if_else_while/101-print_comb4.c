a019753acdd3508861788bb5f6a3623e1e4ac933
usama <usamamoody@gmail.com>
2022-04-09 03:20:21 +0300

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,9 @@ int main(void)
 	m = 49;
 	o = 50;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 49; m <= 58; m++)
+		for (m = 49; m <= 57; m++)
 		{
 			for (o = 50; o <= 58; o++)
 			{
