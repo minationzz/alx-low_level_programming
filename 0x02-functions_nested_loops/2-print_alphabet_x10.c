9ed6b99767f94687b1ff8b96ebd22ee4184bdf47
usama <usamamoody@gmail.com>
2022-04-12 22:19:09 +0300

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void print_alphabet_x10(void);
+
+int b = 0;
+
+while (b < 10)
+{
+
+	char c = 'a';
+
+	while (c < 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+_putchar('\n');
+b++;
+}
+return;
+}
