58116189e3f9c6b789f09b76d13e33c8d2f4579a
usama <usamamoody@gmail.com>
2022-04-14 23:40:59 +0300

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers 0 to 14
+ * followed by a new line (\n)
+ *
+ **/
+
+
+
+
+void more_numbers(void)
+{
+
+int count;
+int num;
+
+count = 0;
+
+while (count = 0; count < 11; count++)
+{
+while (num = 0; num < 15; num++)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
+
