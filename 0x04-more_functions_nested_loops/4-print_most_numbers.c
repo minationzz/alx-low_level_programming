bde0b07ce1331a99e06255b8e3ba4b01dc2ca128
usama <usamamoody@gmail.com>
2022-04-14 23:31:45 +0300

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ **/
+
+void print_most_numbers(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+{
+		if (num != 2 && num != 4)
+
+                _putchar('0' + i);
+}
+        _putchar('\n');
+}
+
+
+
+
