2613dca8d6b907fcebec0edccd74e22b691cc0a3
usama <usamamoody@gmail.com>
2022-04-20 22:58:33 +0300

    7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,4 +15,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 
-#endif // MAIN_H
+#endif
